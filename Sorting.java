de35f627c5cba04530fbc7e99e7698acb14141cd
TKCoder13 <TKCoding13@gmail.com>
2023-02-22 21:06:10 -0800

    insertion and bubble done, modified needs work

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.lang.Math;
 
 public class Sorting {
 
@@ -16,16 +17,20 @@ public class Sorting {
             array[j] = temp;
         }
     }
+
     public static void insertionSort(int[] list, int N) {
         //N = number of elements in list
         count = 0;
-        for (int i = 0; i < N; i++) {
-            int newElement = list[i];
-            int location = i - 1;
+        int newElement;
+        int location;
+        for (int i = 1; i < N; i++) {
+            newElement = list[i];
+            location = i - 1;
             count++;
-            while (location >= 1 && list[location] > newElement) {
+            while (location >= 0 && list[location] > newElement) {
                 list[location + 1] = list[location];
                 location = location - 1;
+                count++;
             }
             list[location + 1] = newElement;
         }
@@ -34,17 +39,19 @@ public class Sorting {
     
 
     public static void bubbleSort(int[] list, int N) {
-        int numOfPairs = N;
+        int numOfPairs = N - 1;
         count = 0;
         boolean swappedElements = true;
         
         while (swappedElements) {
-            numOfPairs = numOfPairs - 1;
+            numOfPairs--;
             swappedElements = false;
-            for (int i = 1; ; i++) {
+            for (int i = 0; i <= numOfPairs; i++) {
                 count++;
                 if (list[i] > list[i + 1]) {
-                    //swap()
+                    int temp = list[i+1];
+                    list[i+1] = list[i];
+                    list[i] = temp;
                     swappedElements = true;
                 }
             }
@@ -52,33 +59,62 @@ public class Sorting {
     }
 
     public static void modifiedBubbleSort(int[] list, int N) {
-        
+        int numOfPairs = N - 1;
+        count = 0;
+        boolean swappedElements = true;
+        int isOdd = 1;
+        int evenI = 0;
+
+        while (swappedElements) {
+            numOfPairs--;
+            swappedElements = false;
+            for (int i = 0; i <= numOfPairs; i++) {
+                count++;
+                if (isOdd % 2 == 1) {
+                    if (list[i] > list[i + 1]) {
+                        int temp = list[i+1];
+                        list[i+1] = list[i];
+                        list[i] = temp;
+                        swappedElements = true;
+                        isOdd++;
+                    }
+                } else {
+                    evenI = numOfPairs;
+                    if (list[evenI] > list[evenI-1]) {
+                        int temp = list[evenI-1];
+                        list[evenI-1] = list[evenI];
+                        list[evenI] = temp;
+                        swappedElements = true;
+                        isOdd++;
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int len = 100;
-        int[] list = new int[len];
-        for (int i = 0; i < len; i++) {
-            list[i] = i;
-        }
+        // int maxExp = 16;
+        // System.out.println("Size\tCount");
 
-        shuffle(list);
-        System.out.println("test");
-        insertionSort(list, len);
-        for (int i = 0; i < len; i++) {
-            System.out.println(i + ": " + list[i]);
+        // for (int i = 0; i <= maxExp; i++) {
+        //     int size = (int) Math.pow(2, i);
+        //     int[] list = new int[size];
+            
+        //     for (int j = 0; j < size; j++) {
+        //         list[j] = j;
+        //     }
+
+        //     shuffle(list);
+        //     bubbleSort(list, size);
+            
+        //     System.out.println(size + "\t" + count);
+        // }
+        
+        int[] example = {6,2,4,7,1,3,8,5};
+        int exampleSize = example.length;
+        modifiedBubbleSort(example, exampleSize);
+        for (int i = 0; i < exampleSize; i++) {
+            System.out.println(example[i]);
         }
-        System.out.println("count " + count);
-        // System.out.println(count);
-        
-        // test list that was provided
-        /*  
-         * int[] testList = {6,2,4,7,1,3,8,5};
-         * // insertionSort(testList, testList.length);
-         * // bubbleSort(testList, testList.length);
-         * // modifiedBubbleSort(testList, testList.length);
-         * System.out.println(counter);
-        */
-        
     }
 }
